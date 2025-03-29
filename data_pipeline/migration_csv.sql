801dbce3b152c7d08377528e992c66ac7df60264
votanasrey <votana.srey@gmail.com>
2025-03-29 09:04:23 +0000

    chore: update migration and mimic script

--- data_pipeline/migration_csv.sql
+++ data_pipeline/migration_csv.sql
@@ -1,8 +1,6 @@
 LOAD CSV FROM '/var/lib/memgraph/minic_data.csv' WITH HEADER AS row
-
 MERGE (sender:Account {account_id: row.account})
 ON CREATE SET sender.bank = row.from_bank
-
 MERGE (receiver:Account {account_id: row["account.1"]})
 ON CREATE SET receiver.bank = row.to_bank
 
@@ -12,9 +10,9 @@ ON CREATE SET
   tr.amount_received = tofloat(row.amount_received),
   tr.receiving_currency = row.receiving_currency,
   tr.amount_paid = tofloat(row.amount_paid),
+  tr.usd_amount = tofloat(row.usd_amount),
   tr.payment_currency = row.payment_currency,
   tr.payment_format = row.payment_format,
   tr.is_laundering = toInteger(row.is_laundering)
-
 MERGE (sender) -[:FROM]->(tr)
 MERGE (tr) -[:TO]->(receiver)
